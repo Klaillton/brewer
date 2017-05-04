24f0ba1b41c690536b26be048a8da010f1c267a7
Klaillton <klaillton@gmail.com>
2017-05-04 16:23:35 -0300

    Melhorando a disponibilidade da aplicação
    Ao realizar uma operação pesada no lado do servidor, a aplicação fica
    com recursos travados o que pode tornar a aplicação por completo lenta,
    divindo a ação em threads, eu libero os recursos do servidor e quando a
    operação terminar o servidor recupera a thread e completa a requisição

--- src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -4,17 +4,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.brewer.storage.FotoStorageRunnable;
 
 @RestController
 @RequestMapping("/fotos")
 public class FotosController {
 
 	@PostMapping
-	public String upload(@RequestParam("files[]") MultipartFile[] files){
-		System.out.println(">>>> Files: "+files[0].getSize());
-		
-		return "Ok!";
+	public DeferredResult<String> upload(@RequestParam("files[]") MultipartFile[] files){
+		DeferredResult<String> resultado = new DeferredResult<>(); /*Melhorar a disponibilidade da aplicação divindindo em threads*/		
+		Thread thread = new Thread(new FotoStorageRunnable(files, resultado));
+		thread.start();
+		return resultado;
 	}
 	
 }
---
+++ src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -0,0 +1,25 @@
+package com.algaworks.brewer.storage;
+
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FotoStorageRunnable implements Runnable {
+	
+	private MultipartFile[] files;
+	private DeferredResult<String> resultado;
+	
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<String> resultado){
+		this.files = files;
+		this.resultado = resultado;
+	}
+
+	@Override
+	public void run() {
+
+		System.out.println(">>>> Files: "+files[0].getSize());
+		//TODO: Salvar a foto no sistema de arquivos... (operação demorada)
+		resultado.setResult("OK! Foto recebida!");
+
+	}
+
+}
