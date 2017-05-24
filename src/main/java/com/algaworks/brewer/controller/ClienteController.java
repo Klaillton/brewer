2377b8a63d2939f27f84ad081b0809494ad878e7
Klaillton <klaillton@gmail.com>
2017-05-24 17:51:31 -0300

    Capitulo 16
    16.1 ao 16.6

--- src/main/java/com/algaworks/brewer/controller/ClienteController.java
+++ src/main/java/com/algaworks/brewer/controller/ClienteController.java
@@ -1,14 +1,26 @@
 package com.algaworks.brewer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.brewer.model.TipoPessoa;
+import com.algaworks.brewer.repository.Estados;
 
 @Controller
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@RequestMapping("/clientes/novo")
-	public String novo() {
-		return "cliente/CadastroCliente";
+	@Autowired
+	private Estados estados;
+	
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
+		mv.addObject("tipoPessoa", TipoPessoa.values());
+		mv.addObject("estados", estados.findAll());
+		return mv;
 	}
 
 }
