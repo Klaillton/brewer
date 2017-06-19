d6170fdcd4480aca329e0687424208bb78fa42d5
Klaillton <klaillton@gmail.com>
2017-06-19 17:10:14 -0300

    Complemento da aula 19.9

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -82,18 +82,22 @@ Brewer.MaskDate = (function() {
 	
 }());
 
-//Brewer.Security = (function(){
-//	
-//	function Security(){
-//		this.token = $('input[name=_csrf]').val();
-//		this.header = $('input[name=_csrf_header]').val();
-//	}
-//	
-//	Security.prototype.enable = function() {
-//		
-//	}
-//	
-//}()):
+Brewer.Security = (function(){
+	
+	function Security(){
+		this.token = $('input[name=_csrf]').val();
+		this.header = $('input[name=_csrf_header]').val();
+	}
+	
+	Security.prototype.enable = function() {
+		$(document).ajaxSend(function (event, jqxhr, settings){
+			jqxhr.setRequestHeader(this.header, this.token);
+		}.bind(this));
+	}
+	
+	return Security;
+	
+}());
 
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
@@ -107,5 +111,8 @@ $(function() {
 	
 	var maskDate = new Brewer.MaskDate();
 	maskDate.enable();
+	
+	var security = new Brewer.Security();
+	security.enable();
 	
 });
