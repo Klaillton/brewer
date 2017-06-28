d9e21e08e0c4431e69d89b76c71f0cdb23bb4a04
Klaillton <klaillton@gmail.com>
2017-06-28 11:03:40 -0300

    trabalhando filtro no grupo

--- src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosImpl.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository.helper.usuario;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,17 +8,19 @@ import javax.persistence.EntityManager;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Subqueries;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.algaworks.brewer.model.Grupo;
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.model.UsuarioGrupo;
 import com.algaworks.brewer.repository.filter.UsuarioFilter;
 import com.algaworks.brewer.repository.paginacao.PaginacaoUtil;
 
@@ -44,41 +47,70 @@ public class UsuariosImpl implements UsuariosQueries{
 				.getResultList();
 	}
 
-	@SuppressWarnings({ "unchecked", "deprecation" })
-	@Override
-	@Transactional(readOnly = true)
-	public Page<Usuario> filtrar(UsuarioFilter filtro, Pageable pageable) {
-		Criteria criteria = manager.unwrap(Session.class).createCriteria(Usuario.class);
-		
-		paginacaoUtil.preparar(criteria, pageable);
-		
-		adicionarFiltro(filtro, criteria);
-		
-		return new PageImpl<>(criteria.list(), pageable, total(filtro));
-	}
 	
-	private Long total(UsuarioFilter filtro) {
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	@Override
+	public List<Usuario> filtrar(UsuarioFilter filtro){
+		
 		@SuppressWarnings("deprecation")
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Usuario.class);
+		
 		adicionarFiltro(filtro, criteria);
-		criteria.setProjection(Projections.rowCount());
-		return (Long) criteria.uniqueResult();
+		
+		return criteria.list();
 	}
-	
+
 	private void adicionarFiltro(UsuarioFilter filtro, Criteria criteria) {
-		if(filtro != null){
-			
-			if (!StringUtils.isEmpty(filtro.getNome())) {
-				criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
-			}
-			
-			if (!StringUtils.isEmpty(filtro.getEmail())) {
-				criteria.add(Restrictions.ilike("email", filtro.getEmail(), MatchMode.ANYWHERE));
+		if(filtro != null) {
+			if(!StringUtils.isEmpty(filtro.getNome())){
+				criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));			
 			}
 
+			if(!StringUtils.isEmpty(filtro.getEmail())){
+				criteria.add(Restrictions.ilike("email", filtro.getEmail(), MatchMode.ANYWHERE));			
+			}
 			
+			if(filtro.getGrupos() != null && filtro.getGrupos().isEmpty()){
+				List<Criterion> subqueries = new ArrayList<>();
+				for(Long codigoGrupo : filtro.getGrupos().stream().mapToLong(Grupo::getCodigo).toArray()){
+					DetachedCriteria dc = DetachedCriteria.forClass(UsuarioGrupo.class);
+					dc.add(Restrictions.eq("id.grupo.codigo", codigoGrupo));
+					dc.setProjection(Projections.property("id.usuario"));
+					
+					subqueries.add(Subqueries.propertyIn("codigo", dc));
+				}
+				
+				Criterion[] criterions = new Criterion[subqueries.size()];
+				criteria.add(Restrictions.and(subqueries.toArray(criterions)));
+			}
 		}
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
