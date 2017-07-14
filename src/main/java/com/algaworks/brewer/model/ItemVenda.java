60b5977eac7c271efee338f8a18349de1fc1e0c0
Klaillton <klaillton@gmail.com>
2017-07-14 18:19:24 -0300

    12 a 14
    Atualizando valor total e desafios

--- src/main/java/com/algaworks/brewer/model/ItemVenda.java
+++ src/main/java/com/algaworks/brewer/model/ItemVenda.java
@@ -2,13 +2,37 @@ package com.algaworks.brewer.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "item_venda")
 public class ItemVenda {
 
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+
 	private Integer quantidade;
+
+	@Column(name = "valor_unitario")
 	private BigDecimal valorUnitario;
+
+	@ManyToOne
+	@JoinColumn(name = "codigo_cerveja")
 	private Cerveja cerveja;
+
+	@ManyToOne
+	@JoinColumn(name = "codigo_venda")
+	private Venda venda;
 	
 	public Long getCodigo() {
 		return codigo;
